03a35db2604363f37dc2437c80b90992155a1536
Nathan Dimmer <nathandimmer@gmail.com>
2020-04-05 10:11:44 -0400

    feat(front-end): added state to the create event page (#8)
    * feat(front-end): tried to add state
    
    * fix(front-end): Fixed Create Event State
    
    Co-authored-by: Andrew Dimmer <contactandrewd@gmail.com>

--- front-end/src/Components/Layouts/DateTime.tsx
+++ front-end/src/Components/Layouts/DateTime.tsx
@@ -1,18 +1,77 @@
-import React, { Fragment } from "react";
-import {
-  Box,
-  Grid,
-  Typography,
-  Button,
-  Card,
-  AppBar,
-  Tabs,
-  Tab,
-  TextField,
-} from "@material-ui/core";
+import { Button, Card, Grid, TextField } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
+import React from "react";
 
-const DateTime: React.FunctionComponent = () => {
+interface DateTimeProps {
+  index: number;
+  dateTime: {
+    startDate: string;
+    startTime: string;
+    endDate: string;
+    endTime: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+  removeDateTime: (index: number) => void;
+  handleDateTimeChange: (
+    input: string,
+    index: number,
+    property:
+      | "startDate"
+      | "startTime"
+      | "endDate"
+      | "endTime"
+      | "address"
+      | "city"
+      | "state"
+      | "zip"
+  ) => void;
+}
+
+const DateTime: React.FunctionComponent<DateTimeProps> = ({
+  index,
+  dateTime,
+  removeDateTime,
+  handleDateTimeChange,
+}) => {
+  const handleStartDateChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    handleDateTimeChange(event.target.value, index, "startDate");
+  };
+
+  const handleStartTimeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    handleDateTimeChange(event.target.value, index, "startTime");
+  };
+
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "endDate");
+  };
+
+  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "endTime");
+  };
+
+  const handleAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "address");
+  };
+
+  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "city");
+  };
+
+  const handleStateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "state");
+  };
+
+  const handleZipChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDateTimeChange(event.target.value, index, "zip");
+  };
+
   return (
     <Card style={{ padding: "1%" }}>
       <Grid container direction="row" alignItems="center">
@@ -23,17 +82,21 @@ const DateTime: React.FunctionComponent = () => {
                 <Grid item xs={6} style={{ paddingRight: "3%" }}>
                   <TextField
                     id="startDate"
+                    value={dateTime.startDate}
                     label="Start date (mm/dd/yyyy)"
                     variant="outlined"
                     fullWidth
+                    onChange={handleStartDateChange}
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField
                     id="startTime"
+                    value={dateTime.startTime}
                     label="Start time (hh:mm AM/PM)"
                     variant="outlined"
                     fullWidth
+                    onChange={handleStartTimeChange}
                   />
                 </Grid>
               </Grid>
@@ -44,17 +107,21 @@ const DateTime: React.FunctionComponent = () => {
                 <Grid item xs={6} style={{ paddingRight: "3%" }}>
                   <TextField
                     id="endDate"
+                    value={dateTime.endDate}
                     label="End date (mm/dd/yyyy)"
                     variant="outlined"
                     fullWidth
+                    onChange={handleEndDateChange}
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField
                     id="endTime"
+                    value={dateTime.endTime}
                     label="End time (hh:mm AM/PM)"
                     variant="outlined"
                     fullWidth
+                    onChange={handleEndTimeChange}
                   />
                 </Grid>
               </Grid>
@@ -64,33 +131,41 @@ const DateTime: React.FunctionComponent = () => {
                 <Grid item xs={6} style={{ paddingRight: "1%" }}>
                   <TextField
                     id="address"
+                    value={dateTime.address}
                     label="Street address"
                     variant="outlined"
                     fullWidth
+                    onChange={handleAddressChange}
                   />
                 </Grid>
                 <Grid item xs={2} style={{ paddingRight: "1%" }}>
                   <TextField
                     id="city"
+                    value={dateTime.city}
                     label="City"
                     variant="outlined"
                     fullWidth
+                    onChange={handleCityChange}
                   />
                 </Grid>
                 <Grid item xs={2} style={{ paddingRight: "1%" }}>
                   <TextField
                     id="state"
+                    value={dateTime.state}
                     label="State"
                     variant="outlined"
                     fullWidth
+                    onChange={handleStateChange}
                   />
                 </Grid>
                 <Grid item xs={2}>
                   <TextField
                     id="postalCode"
-                    label="Postal code"
+                    value={dateTime.zip}
+                    label="Zip Code"
                     variant="outlined"
                     fullWidth
+                    onChange={handleZipChange}
                   />
                 </Grid>
               </Grid>
@@ -98,7 +173,13 @@ const DateTime: React.FunctionComponent = () => {
           </Grid>
         </Grid>
         <Grid item>
-          <Button>
+          <Button
+            onClick={() => {
+              console.log(index);
+              console.log(removeDateTime);
+              removeDateTime(index);
+            }}
+          >
             <CloseIcon />
           </Button>
         </Grid>
--- front-end/src/Components/Pages/CreateEvent.tsx
+++ front-end/src/Components/Pages/CreateEvent.tsx
@@ -1,21 +1,104 @@
-import React, { Fragment } from "react";
 import {
   Box,
-  Grid,
-  Typography,
   Button,
   Card,
-  AppBar,
-  Tabs,
-  Tab,
-  TextField,
-  FormControlLabel,
   Checkbox,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
 } from "@material-ui/core";
-import DateTime from "../Layouts/DateTime";
+import React, { Fragment } from "react";
 import categories from "../Content/Categories";
+import DateTime from "../Layouts/DateTime";
 
 const CreateEvent: React.FunctionComponent = () => {
+  const [eventName, setEventName] = React.useState<string>();
+  const [eventDescription, setEventDescription] = React.useState<string>();
+  const [contactInfo, setContactInfo] = React.useState<string>();
+  const [dateTimes, setDateTimes] = React.useState<
+    {
+      startDate: string;
+      startTime: string;
+      endDate: string;
+      endTime: string;
+      address: string;
+      city: string;
+      state: string;
+      zip: string;
+    }[]
+  >([
+    {
+      startDate: "",
+      startTime: "",
+      endDate: "",
+      endTime: "",
+      address: "",
+      city: "",
+      state: "",
+      zip: "",
+    },
+  ]);
+  const [categorySelections, setCategorySelections] = React.useState();
+
+  const handleEventNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEventName(event.target.value);
+  };
+
+  const handleEventDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEventDescription(event.target.value);
+  };
+
+  const handleContactInfoChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setContactInfo(event.target.value);
+  };
+
+  const handleDateTimeChange = (
+    value: string,
+    index: number,
+    property:
+      | "startDate"
+      | "startTime"
+      | "endDate"
+      | "endTime"
+      | "address"
+      | "city"
+      | "state"
+      | "zip"
+  ) => {
+    const newDateTimes = dateTimes.concat([]);
+    newDateTimes[index][property] = value;
+    setDateTimes(newDateTimes);
+  };
+
+  const removeDateTime = (index: number) => {
+    const newDateTimes = dateTimes.concat([]);
+    newDateTimes.splice(index, 1);
+    setDateTimes(newDateTimes);
+  };
+
+  const addDateTime = () => {
+    const newDateTimes = dateTimes.concat([
+      {
+        startDate: "",
+        startTime: "",
+        endDate: "",
+        endTime: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+      },
+    ]);
+    setDateTimes(newDateTimes);
+  };
+
   return (
     <Fragment>
       <Box mx="5%" my="2.5%">
@@ -27,33 +110,55 @@ const CreateEvent: React.FunctionComponent = () => {
             <Grid item xs={12}>
               <TextField
                 id="eventName"
+                value={eventName}
                 label="Event Name"
                 variant="outlined"
                 fullWidth
+                onChange={handleEventNameChange}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 id="eventDescription"
+                value={eventDescription}
                 label="Event Description"
                 variant="outlined"
                 multiline
                 fullWidth
+                onChange={handleEventDescriptionChange}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 id="organizerConctact"
+                value={contactInfo}
                 label="Organizer Contact Information"
                 variant="outlined"
                 fullWidth
+                onChange={handleContactInfoChange}
               />
             </Grid>
             <Grid item>
-              <DateTime></DateTime>
+              {dateTimes.map((value, index) => {
+                return (
+                  <DateTime
+                    index={index}
+                    dateTime={value}
+                    removeDateTime={removeDateTime}
+                    handleDateTimeChange={handleDateTimeChange}
+                  ></DateTime>
+                );
+              })}
+
               {/* TODO: Add other datetimes here */}
               <br />
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  addDateTime();
+                }}
+              >
                 New date
               </Button>
             </Grid>
@@ -67,7 +172,7 @@ const CreateEvent: React.FunctionComponent = () => {
               <Grid container direction="column">
                 {categories.map((val, index) => {
                   return (
-                    <Grid item>
+                    <Grid item key={index}>
                       <FormControlLabel
                         control={
                           <Checkbox
