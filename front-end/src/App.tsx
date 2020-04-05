6a51a696d06fe4f3f9923ff4036039d5fd92d0f4
Andrew Dimmer <contactandrewd@gmail.com>
2020-04-04 20:36:27 -0400

    feat(front-end): Created Initial Page Layout

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,28 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles
+} from "@material-ui/core";
+import React, { Fragment } from "react";
+import NavBar from "./Components/Layouts/NavBar";
 import LoadingScreen from "./Components/Misc/LoadingScreen";
+import NotificationBar, {
+  NotificationMessage
+} from "./Components/Misc/Notifications";
+import HomePage from "./Components/Pages/HomePage";
+import { darkTheme, lightTheme } from "./Styles/theme";
+import { useDarkMode } from "./Styles/useDarkMode";
+import { styles } from "./Styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <LoadingScreen busyMessage="" />
-    </div>
+const App: React.FunctionComponent = () => {
+  const { theme, toggleTheme, componentMounted } = useDarkMode();
+  const [notification, setNotification] = React.useState<NotificationMessage>({
+    type: "info",
+    message: "Notifications are working!",
+    open: true
+  });
+  const [loadingMessage, setLoadingMessage] = React.useState<string>("");
+
+  const localStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      marginedTop: {
+        marginTop: theme.spacing(2)
+      }
+    })
   );
-}
+
+  const localClasses = localStyles();
+
+  if (!componentMounted) {
+    return <Fragment />;
+  }
+
+  return (
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <NavBar theme={theme} toggleTheme={toggleTheme} user={null} />
+      <Container className={localClasses.marginedTop}>
+        <HomePage />
+      </Container>
+      <LoadingScreen loadingMessage={loadingMessage} />
+      <NotificationBar
+        notification={notification}
+        setNotification={setNotification}
+      />
+    </ThemeProvider>
+  );
+};
 
 export default App;
